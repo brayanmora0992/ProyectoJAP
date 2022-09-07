5ecc6fe6b37c1a8f6908c3c6d766f123fdbea632
brayanmora0992 <brayanmora0992@gmail.com>
2022-09-07 14:56:22 -0300

    cambios menores

--- js/product-info.js
+++ js/product-info.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', ()=>{
     fetch(productInfo)
     .then(response => response.json())
     .then(datos => {
-            
         //preguntar como lo hago con un for loop
             infoContainer.innerHTML = `
             <div class="d-flex flex-row justify-content-between shadow p-5 mb-5 bg-body rounded border-0" id="cuerpoProducto">
@@ -27,20 +26,23 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 <p>${datos.soldCount}</p>
               </div>
               <hr>
+              <div>
+                <h5>Precio: <span>${datos.currency} ${datos.cost}</span></h5>
+              </div>
             </div>
-            <div id="carouselControls" class="carousel carousel-dark slide w-50" data-bs-ride="carousel" >
+            <div id="carouselControls" class="carousel carousel-dark slide w-100" data-bs-ride="carousel" >
               <div class="carousel-inner" id="imgContainer">
                 <div class="carousel-item active">
-                  <img src="${datos.images[0]}"  alt="${datos.name[0]}">
+                  <img src="${datos.images[0]}" alt="${datos.name[0]}">
               </div>
               <div class="carousel-item">
-                  <img src="${datos.images[1]}"  alt="${datos.name[1]}">
+                  <img src="${datos.images[1]}" alt="${datos.name[1]}">
               </div>
               <div class="carousel-item">
-                  <img src="${datos.images[2]}"  alt="${datos.name[2]}">
+                  <img src="${datos.images[2]}" alt="${datos.name[2]}">
               </div>  
               <div class="carousel-item">
-                  <img src="${datos.images[3]}"  alt="${datos.name[3]}">
+                  <img src="${datos.images[3]}" alt="${datos.name[3]}">
               </div>
               </div>
               <button class="carousel-control-prev" type="button" data-bs-target="#carouselControls" data-bs-slide="prev">
