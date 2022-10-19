be4df348e755d92acab4460f47a6dfd8dfa51867
brayanmora0992 <brayanmora0992@gmail.com>
2022-10-19 13:55:26 -0300

    selector de envíos funcionando

--- js/cart.js
+++ js/cart.js
@@ -10,7 +10,7 @@ function mostrarItems(arreglo){
             <td>${arreglo.articles[i].name}</td>
             <td>${arreglo.articles[i].currency} <span class="costoUnitario">${arreglo.articles[i].unitCost}</span></td>
             <td><input class="form-control cantidades" type="number" onchange="calcular()" value="${arreglo.articles[i].count}" id="cantidad" min="0"></td>
-            <td><strong>${arreglo.articles[i].currency} <span class="subtotales">${arreglo.articles[i].unitCost}</span></strong></td>
+            <td><strong><span class="tipoCambio">${arreglo.articles[i].currency}</span> <span class="subtotales">${arreglo.articles[i].unitCost}</span></strong></td>
         </tr>` 
     }
 }
@@ -28,7 +28,7 @@ function mostrarProductosAgregadosAlCarrito(producto){
             <td>${producto[i].name}</td>
             <td>${producto[i].currency} <span class="costoUnitario">${producto[i].cost}</span></td>
             <td><input class="form-control cantidades" type="number" onchange="calcular()" value="1" id="cantidad" min="0"></td>
-            <td><strong>${producto[i].currency} <span class="subtotales">${producto[i].cost}</span></strong></td>
+            <td><strong><span class="tipoCambio">${producto[i].currency}</span> <span class="subtotales">${producto[i].cost}</span></strong></td>
         </tr>` 
         
     }
@@ -38,19 +38,45 @@ function mostrarProductosAgregadosAlCarrito(producto){
 function calcular(){
     let costoUnitario = document.getElementsByClassName('costoUnitario');
     let subtotales = document.getElementsByClassName('subtotales');
-    let cantidades = document.getElementsByClassName('cantidades')
-    let valorTotal = 0;
-    let subtotalIndividual = 0;
-    
+    let cantidades = document.getElementsByClassName('cantidades');
+    let tipoCambio = document.getElementsByClassName('tipoCambio');
+    let valorEnvio = document.getElementById('valorEnvio');
+    let valorSubtotal = 0;
+    let costoEnvio = 0
+
     for (let i = 0; i < costoUnitario.length; i++) {
-        subtotalIndividual += parseInt(costoUnitario[i].innerHTML) * parseInt(cantidades[i].value)
         subtotales[i].innerHTML = parseInt(costoUnitario[i].innerHTML) * parseInt(cantidades[i].value)
-        valorTotal += parseInt(subtotales[i].innerHTML)
-        
-        document.getElementById('total').innerHTML = `Valor total: <strong>${valorTotal}</strong>`
-        
-        
     }
+
+    for (let x = 0; x < tipoCambio.length; x++) {
+        if (tipoCambio[x].innerHTML !== "USD") {
+            valorSubtotal += (parseInt(costoUnitario[x].innerHTML) / 40) * parseInt(cantidades[x].value)
+        } else {
+            valorSubtotal += (parseInt(costoUnitario[x].innerHTML)) * parseInt(cantidades[x].value)
+            
+        }
+        
+        document.getElementById('total').innerHTML = `USD ${Math.round(valorSubtotal)}</strong>`
+        // evalúo que envío está seleccionado
+        if (document.getElementById('standard').checked) {
+            costoEnvio = valorSubtotal * 0.05
+            valorEnvio.innerHTML = `USD ${Math.round(costoEnvio)}`
+            document.getElementById('costoTotal').innerHTML = `<strong>USD ${Math.round(valorSubtotal + costoEnvio)}</strong>`;
+
+        } else if (document.getElementById('express').checked){
+            costoEnvio = valorSubtotal * 0.07
+            valorEnvio.innerHTML = `USD ${Math.round(costoEnvio)}`
+            document.getElementById('costoTotal').innerHTML = `<strong>USD ${Math.round(valorSubtotal + costoEnvio)}</strong>`;
+
+        } else {
+            costoEnvio = valorSubtotal * 0.15
+            valorEnvio.innerHTML = `USD ${Math.round(costoEnvio)}`
+            document.getElementById('costoTotal').innerHTML = `<strong>USD ${Math.round(valorSubtotal + costoEnvio)}</strong>`;
+
+        }
+
+    }
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -59,6 +85,13 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(items => {
         mostrarItems(items)
         traerProductoLocalStorage()
+        calcular()
+    })
+
+    document.getElementById('express').addEventListener('click', ()=>{
+        calcular()
+    })
+    document.getElementById('premium').addEventListener('click', ()=>{
         calcular()
     })
     
