f43f6a9209a152e2ffb862a43cf44db703912f75
brayanmora0992 <brayanmora0992@gmail.com>
2022-10-21 14:26:30 -0300

    cambios en la validación del formulario, falta validación en ventana modal

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,24 @@
 const direccion = `https://japceibal.github.io/emercado-api/user_cart/25801.json`;
+const formularios = document.querySelectorAll('.needs-validation')
 let listaProductos = document.getElementById('listaProductos')
+let transfBancaria = document.getElementById('transfbancaria');
+let tarjetaCredito = document.getElementById('tarjetaCredito');
+let medioPagoSelec = document.getElementById('medioPagoSelec');
+let campoNumTarjeta = document.getElementById('num');
+let codigoSeg = document.getElementById('codigo');
+let vencimiento = document.getElementById('vencimiento');
+let campoNumCuenta = document.getElementById('numCuenta');
 let carrito = [];
+
+function verificarCamposModal(){
+    let validity = true;
+    if (!campoNumTarjeta.checkValidity() || !vencimiento.checkValidity() || !codigoSeg.checkValidity() || !transfBancaria.checkValidity()) {
+        campoNumCuenta.setCustomValidity(false)
+        validity = false;
+
+    }
+}
+
 
 function mostrarItems(arreglo){
     for (let i = 0; i < arreglo.articles.length; i++) {
@@ -9,7 +27,7 @@ function mostrarItems(arreglo){
             <td class="col-2"><img src="${arreglo.articles[i].image}" class="img-thumbnail"</td>
             <td>${arreglo.articles[i].name}</td>
             <td>${arreglo.articles[i].currency} <span class="costoUnitario">${arreglo.articles[i].unitCost}</span></td>
-            <td><input class="form-control cantidades" type="number" onchange="calcular()" value="${arreglo.articles[i].count}" id="cantidad" min="0"></td>
+            <td><input class="form-control cantidades" type="number" onchange="calcular()" value="${arreglo.articles[i].count}" id="cantidad" min="1"></td>
             <td><strong><span class="tipoCambio">${arreglo.articles[i].currency}</span> <span class="subtotales">${arreglo.articles[i].unitCost}</span></strong></td>
         </tr>` 
     }
@@ -27,7 +45,7 @@ function mostrarProductosAgregadosAlCarrito(producto){
             <td class="col-2"><img src="${producto[i].images[0]}" class="img-thumbnail"</td>
             <td>${producto[i].name}</td>
             <td>${producto[i].currency} <span class="costoUnitario">${producto[i].cost}</span></td>
-            <td><input class="form-control cantidades" type="number" onchange="calcular()" value="1" id="cantidad" min="0"></td>
+            <td><input class="form-control cantidades" type="number" onchange="calcular()" value="1" id="cantidad" min="1"></td>
             <td><strong><span class="tipoCambio">${producto[i].currency}</span> <span class="subtotales">${producto[i].cost}</span></strong></td>
         </tr>` 
         
@@ -79,10 +97,51 @@ function calcular(){
 
 }
 
+function mostrarMedioDePago(){
+
+    if (transfBancaria.checked) {
+        medioPagoSelec.innerHTML = 'Transferencia bancaria';
+    } else if (tarjetaCredito.checked){
+        medioPagoSelec.innerHTML = 'Tarjeta de crédito';  
+   
+    }
+}
+
+function deshabilitarInputs(){
+
+    if (transfBancaria.checked) {
+        campoNumTarjeta.disabled = true;
+        codigoSeg.disabled = true; 
+        vencimiento.disabled = true;
+        campoNumCuenta.disabled = false;
+    } else if (tarjetaCredito.checked){
+        campoNumCuenta.disabled = true;   
+        campoNumTarjeta.disabled = false;
+        codigoSeg.disabled = false; 
+        vencimiento.disabled = false;   
+    }
+
+}
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    let transfBancaria = document.getElementById('transfbancaria');
-    let tarjetaCredito = document.getElementById('tarjetaCredito');
-    let medioPagoSelec = document.getElementById('medioPagoSelec');
+
+/* A function that is called when the form is submitted. It checks if the form is valid and if it is
+not, it prevents the default action and stops the propagation of the event. It also calls the
+function validacion(). */
+    Array.prototype.slice.call(formularios)
+    .forEach(function (formularios) {
+      formularios.addEventListener('submit', function (event) {
+        if (!formularios.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+          verificarCamposModal()
+        } 
+
+        formularios.classList.add('was-validated')
+      }, false)
+    })
 
     fetch(direccion)
     .then(response => response.json())
@@ -98,12 +157,18 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById('premium').addEventListener('click', ()=>{
         calcular()
     })
+
+    document.getElementById('transfbancaria').addEventListener('click', ()=>{
+        deshabilitarInputs()
+    })
+
+    document.getElementById('tarjetaCredito').addEventListener('click', ()=>{
+        deshabilitarInputs()
+    })
     
     document.getElementById('cerrar').addEventListener('click',()=>{
-        if (transfBancaria.checked) {
-            medioPagoSelec.innerHTML = 'Transferencia bancaria';
-        } else if (tarjetaCredito.checked){
-            medioPagoSelec.innerHTML = 'Tarjeta de crédito';        
-        }
+        mostrarMedioDePago()
     })
+
+    
 });
